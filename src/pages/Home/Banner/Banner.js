22ff93c4ada06de5ff8063dfd43e0c7beaa4e61a
Toriqul-Leon <yeitoriqul@gmail.com>
2022-04-18 02:51:38 +0600

    Random Commit. So far so good

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Banner.css";
 import { Carousel } from "react-bootstrap";
 import slide1 from "../../../../src/images/slide-1.jpeg";
 import slide2 from "../../../../src/images/slide-2.jpg";
@@ -8,28 +9,17 @@ const Banner = () => {
   return (
     <div className="banner">
       <Carousel>
-        <Carousel.Item interval={1000}>
+        <Carousel.Item className="carousel-item" interval={1000}>
           <img className="d-block w-100" src={slide1} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+          <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={500}>
+        <Carousel.Item className="carousel-item" interval={500}>
           <img className="d-block w-100" src={slide2} alt="Second slide" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="carousel-item">
           <img className="d-block w-100" src={slide3} alt="Third slide" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
+          <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
